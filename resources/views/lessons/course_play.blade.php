e8028769d99d224884fd446e888467c22bf3b539
Henry Augusta <henryaugusta4@gmail.com>
2023-07-14 09:02:53 +0700

    add autoplay

--- resources/views/lessons/course_play.blade.php
+++ resources/views/lessons/course_play.blade.php
@@ -102,7 +102,7 @@
                 </div>
 
                 <div class="container-fluid">
-                    <video crossorigin playsinline id="playVideo168" width="100%" class="video-mask"
+                    <video crossorigin playsinline id="playVideo168" autoplay="autoplay" width="100%" class="video-mask"
                         disablePictureInPicture controlsList="nodownload">
                         <!-- Video files -->
                         <source
