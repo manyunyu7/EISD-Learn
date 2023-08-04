3136755a769a65f49d8cc826c079275b92804345
Henry Augusta <henryaugusta4@gmail.com>
2023-08-04 11:18:58 +0700

    ~

--- resources/views/lessons/edit_lesson.blade.php
+++ resources/views/lessons/edit_lesson.blade.php
@@ -118,6 +118,12 @@
                                 <small class="form-text text-muted"><strong>Kosongkan Input Ini</strong> jika kelas ini
                                     bisa diakses setiap saat oleh siswa</small>
                                 <div class="form-group">
+                                    @php
+                                        // Convert the database date to the correct format for datetime-local input
+                                        if($lesson->start_date!=null){
+                                        $start_date = date('Y-m-d\TH:i', strtotime($lesson->start_date));
+                                        }
+                                    @endphp
                                     <label>Tanggal Start</label>
                                     <div class="input-group">
                                         <input type="datetime-local" class="form-control"
@@ -128,7 +134,13 @@
                                 <div class="form-group">
                                     <label>Tanggal Selesai</label>
                                     <div class="input-group">
-                                        <input type="datetime-local" value="{{$lesson->end_date}}" class="form-control" name="end_time">
+                                        @php
+                                            // Convert the database date to the correct format for datetime-local input
+                                            if($lesson->end_date!=null){
+                                                $end_date = date('Y-m-d\TH:i', strtotime($lesson->end_date));
+                                            }
+                                        @endphp
+                                        <input type="datetime-local" value="{{ $end_date }}" class="form-control" name="end_time">
                                     </div>
                                 </div>
 
