1294184d905046713b89887e7b86e54345696680
Henry Augusta <henryaugusta4@gmail.com>
2023-10-20 14:09:02 +0700

    add bearer (test)

--- app/Http/Controllers/MobileUploaderController.php
+++ app/Http/Controllers/MobileUploaderController.php
@@ -9,6 +9,7 @@ class MobileUploaderController extends Controller
 {
     public function upload(Request $request)
     {
+        return $request->all();
         // Validate the uploaded files
 //        $request->validate([
 //            'files.*' => 'required|file|mimes:jpeg,png,pdf,doc,docx',
@@ -27,20 +28,19 @@ class MobileUploaderController extends Controller
 
         // Prepare data for Guzzle
         $data = [
-            'site_id' => '7b357c45-ec72-4c14-8695-3b0b207bf13d',
-            'sub_location_site_id' => '5073c341-ad63-47d2-b0ad-e3a3c9521174',
-            'type_id' => '49d83fa9-d7d5-4ff5-9755-7bd3de364d9e',
-            'description' => '168',
+            'site_id' => $request->site_id,
+            'sub_location_site_id' => $request->sub_location_site_id,
+            'type_id' => $request->type_id,
+            'description' => $request->description,
         ];
-
         // Send files using Guzzle
         $client = new Client();
-
+        $bearer = $request->bearer();
         try {
             $response = $client->post('https://api-ithub.modernland.co.id/api/v1/helpdesk', [
                 'headers' => [
                     'Accept' => 'application/json',
-                    'Authorization' => 'Bearer U2FsdGVkX1/tuR/8hFUwcNH5mw0LFO7QLIMntNUxIAenpzYCbcO+IhmxmFk5SlDv+5JXSGdL7TAe3IqkILliC/w0RheTb4OmZL+rOSxd0QiNBkn8JFmIEHqoeiiJ2vprMr3d59NllB2Nw/RD+ZkJpETkZ3S30oyni749rnxQNQrSPhfH9LRSQiaPFJTRiPhb0EvkfZz/gTLSxOMvdtHC6qAmA3NVGHS/NFaaFd/BWC024RyfrpLW1FZGVNc4xuYFGNBW0/vOVZQs79hvIvOWgp7097+CQjjzxOGOBAOt0SuZcUdwsrAVWjf4yYtS3qkD',
+                    'Authorization' => 'Bearer $bearer',
                 ],
                 'multipart' => array_merge(
                     $this->buildMultipartData($data),
