5f6f775d925d670dc7764e1bff9982227727758c
Henry Augusta <henryaugusta4@gmail.com>
2023-10-25 15:25:47 +0700

    add close ticket endpoint.

--- app/Http/Controllers/MobileUploaderController.php
+++ app/Http/Controllers/MobileUploaderController.php
@@ -66,6 +66,62 @@ class MobileUploaderController extends Controller
         }
     }
 
+    public function closeTicket(Request $request)
+    {
+        // Validate the uploaded files
+//        $request->validate([
+//            'files.*' => 'required|file|mimes:jpeg,png,pdf,doc,docx',
+//        ]);
+
+        $url = "";
+        $ticketId = $request->ticketId;
+        if($request->dev=="true"){
+            $url = "https://github.modernland.co.id/api/v1/helpdesk/request-close-ticket/$ticketId";
+        }else{
+            $url = "https://api-ithub.modernland.co.id/api/v1/helpdesk/request-close-ticket/$ticketId";
+        }
+
+        // Get the uploaded files
+        $files = $request->file('files');
+
+        // Move each file to the public/IsengIsengAja directory
+        $uploadedFiles = [];
+        foreach ($files as $file) {
+            $filename = time() . '_' . $file->getClientOriginalName();
+            $file->storeAs('public/IsengIsengAja', $filename);
+            $uploadedFiles[] = $filename;
+        }
+
+        // Prepare data for Guzzle
+        $data = [
+            'message' => $request->message,
+        ];
+        // Send files using Guzzle
+        $client = new Client();
+        $bearer = $request->bearer;
+        try {
+            $response = $client->post($url, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => "Bearer $bearer",
+                ],
+                'multipart' => array_merge(
+                    $this->buildMultipartData($data),
+                    $this->buildFileData($uploadedFiles)
+                ),
+            ]);
+
+            $response = $response->getBody()->getContents();
+
+            // You can handle the response here
+
+            return response()->json(['message' => 'Files uploaded successfully', 'files' => $uploadedFiles]);
+        } catch (\Exception $e) {
+            // Handle errors here
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
     private function buildMultipartData(array $data)
     {
         $multipartData = [];
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('/upload', 'MobileUploaderController@upload');
+Route::post('/close-ticket', 'MobileUploaderController@closeTicket');
 
