ba705b3f439b03e6b1a15e41889aff8cda3f67e9
Henry Augusta <henryaugusta4@gmail.com>
2023-11-16 08:58:15 +0700

    fix migration.

--- database/migrations/2023_08_09_070450_add_acces_flag_to_course_section_view.php
+++ database/migrations/2023_08_09_070450_add_acces_flag_to_course_section_view.php
@@ -24,6 +24,14 @@ class AddAccesFlagToCourseSectionView extends Migration
      */
     public function down()
     {
-        DB::statement("DROP VIEW view_course_section");
+        DB::statement("CREATE OR REPLACE VIEW `view_course_section`
+        AS SELECT a.id as `lesson_id`, a.course_title as lessons_title, a.mentor_id,
+         b.name as `mentor_name`, c.id as `section_id`,
+         c.section_order, c.section_title, c.section_content,
+          c.section_video, c.created_at, c.updated_at
+          from course_section c
+          LEFT JOIN lessons a on a.id=c.course_id LEFT JOIN users b on a.mentor_id=b.id");
+
+        // Note: The `can_be_accessed` column is effectively removed by recreating the view without including it.
     }
 }
