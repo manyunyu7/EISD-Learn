7f9db13379d787ecab72ac878a69cedfe4d43cf6
Henry Augusta <henryaugusta4@gmail.com>
2023-12-06 15:18:21 +0700

    initial absensi model and migration.

---
+++ database/migrations/2023_12_06_043756_create_absensis_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsensisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absensis', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('section_id');
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->foreign('section_id')->references('id')->on('course_section');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absensis');
+    }
+}
