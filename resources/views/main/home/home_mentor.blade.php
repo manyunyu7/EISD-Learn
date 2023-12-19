b38818f009fdec168f6d22b938b6a35db1ff7cc9
Henry Augusta <henryaugusta4@gmail.com>
2023-12-19 08:47:05 +0700

    change app color based on figma.

--- resources/views/main/home/home_mentor.blade.php
+++ resources/views/main/home/home_mentor.blade.php
@@ -55,7 +55,7 @@
 @endsection
 @section('main')
 
-<div class="panel-header bg-primary-gradient">
+<div class="panel-header bg-danger-gradient">
     <div class="page-inner py-5">
         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
             <div>
