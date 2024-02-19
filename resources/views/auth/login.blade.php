1cf78881d5cce85e77e912f6c9caeee9418ee30d
Henry Augusta <henryaugusta4@gmail.com>
2024-02-19 09:25:33 +0700

    168 : Fix Login Layout.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,98 @@
 @extends('auth.template')
 @section('body')
-        <div class="row h-100">
-            <div class="col-sm-12 my-auto">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card" style="border-radius: 30px;">
-                            <div class="mt-4">
-                                <div>
-                                    <h1 style="text-align: center"><b>Login</b></h1>
-                                </div>
-                            </div>
+    <div class="row h-100">
+        <div class="col-sm-12 my-auto">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card" style="border-radius: 30px;">
 
-                            <div class="card-body d-flex justify-content-center align-items-center mb-5">
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <div class="mb-3">
-                                        <label for="exampleInputEmail1" class="form-label">Username</label>
-                                        <input 
-                                            type="email" 
-                                            class="username form-control custom-width @error('email') is-invalid @enderror" 
-                                            id="exampleInputEmail1" 
-                                            aria-describedby="emailHelp" 
-                                            placeholder="Username or Email Address"
-                                            name="email"
-                                            value="{{ old('email') }}" 
-                                            required 
-                                            autocomplete="email" 
-                                            autofocus
-                                            
-                                        >
-                                        @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                                        <div class="input-group">
-                                            <input 
-                                                type="password" 
-                                                class="password form-control @error('password') is-invalid @enderror" 
-                                                id="exampleInputPassword1" 
-                                                placeholder="Password"
-                                                name="password"
-                                                required 
-                                                autocomplete="current-password"
-                                            >
-                                            <button class="btn btn-outline-secondary" type="button" id="togglePassword">
-                                                <i class="fa fa-eye"></i>
-                                            </button>
-                                        </div>
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    <div class="mb-3 form-check d-flex justify-content-start">
-                                        <div class="form-check form-switch">
-                                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
-                                            <label class="form-check-label" for="flexSwitchCheckChecked">Remember me</label>
-                                        </div>
-                                    </div>
-                                    <div class="mb-3 ">
-                                        <button type="submit" class="btn btn-danger btn-block ">Submit</button>
-                                    </div>
-                                    <div class="mb-3 d-flex justify-content-end">
-                                        <a class="btn btn-link link_forgotPwd" href="{{ url('forgotpass') }}" style="color: #FF1D01;">
-                                            {{ __('Forgot Password?') }}
-                                        </a>
-                                    </div>
-                                </form>
-                            </div>
+                        <div class="mt-4">
+
                         </div>
-                        
+
+                        <div class="card-body  justify-content-center align-items-center mb-5">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <h3 class="form-label font-inter text-center">Login</h3>
+                                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                                    <input
+                                        type="email"
+                                        class="username form-control custom-width @error('email') is-invalid @enderror"
+                                        id="exampleInputEmail1"
+                                        aria-describedby="emailHelp"
+                                        placeholder="Username or Email Address"
+                                        name="email"
+                                        value="{{ old('email') }}"
+                                        required
+                                        autocomplete="email"
+                                        autofocus>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                    @enderror
+                                </div>
+                                <div class="mb-3">
+
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                                    <div class="input-group">
+                                        <input
+                                            type="password"
+                                            class="password form-control @error('password') is-invalid @enderror"
+                                            id="exampleInputPassword1"
+                                            placeholder="Password"
+                                            name="password"
+                                            required
+                                            autocomplete="current-password"
+                                        >
+                                        <button class="btn btn-outline-secondary" type="button" id="togglePassword">
+                                            <i class="fa fa-eye"></i>
+                                        </button>
+                                    </div>
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                    @enderror
+                                </div>
+                                <div class="mb-3 form-check d-flex justify-content-start">
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked"
+                                               checked>
+                                        <label class="form-check-label" for="flexSwitchCheckChecked">Remember me</label>
+                                    </div>
+                                </div>
+                                <div class="mb-3 ">
+                                    <button type="submit" class="btn btn-danger btn-block ">Submit</button>
+                                </div>
+                                <div class="mb-3 d-flex justify-content-end">
+                                    <a class="btn btn-link link_forgotPwd" href="{{ url('forgotpass') }}"
+                                       style="color: #FF1D01;">
+                                        {{ __('Forgot Password?') }}
+                                    </a>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div><br>
-                <div class="col-md-12 centere" style="display: flex; justify-content: center;">
-                    <img 
-                        src="{{URL::to('/')}}/home_assets/img/ic_LearningMDLN.svg" 
-                        style=" width:35%; 
+
+                </div>
+            </div>
+            <br>
+            <div class="col-md-12 centere" style="display: flex; justify-content: center;">
+                <img
+                    src="{{URL::to('/')}}/home_assets/img/ic_LearningMDLN.svg"
+                    style=" width:35%;
                                 height: auto;
                                 display: flex;
                                 align-items: center;
                                 "
-                    >
-                </div>
+                >
             </div>
         </div>
+    </div>
 @endsection
 
 
--- resources/views/auth/template.blade.php
+++ resources/views/auth/template.blade.php
@@ -11,12 +11,16 @@
     {{-- IMPROVE BY KEVIN --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.18.0/font/bootstrap-icons.css">
-    
+
     {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integritas="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonim" > --}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -33,6 +37,10 @@
 
         .container {
             height: 100% !important;
+        }
+
+        .font-inter {
+            font-family: 'Inter', sans-serif;
         }
 
         @font-face {
@@ -58,8 +66,8 @@
         .password{
             flex: 10 0 30rem;
         }
-        
-        
+
+
 
     </style>
     <!-- Bootstrap CSS -->
@@ -86,7 +94,7 @@
             var passwordInput = document.getElementById('exampleInputPassword1');
             var type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
             passwordInput.setAttribute('type', type);
-    
+
             var eyeIcon = document.querySelector('#togglePassword i');
             eyeIcon.classList.toggle('bi-eye');
             eyeIcon.classList.toggle('bi-eye-slash');
