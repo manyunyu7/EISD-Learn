6ee286d1ed704b01804695e783714499e55307c6
Henry Augusta <henryaugusta4@gmail.com>
2024-03-19 19:18:10 +0700

    Fix UI on Manage Lesson V2

--- resources/views/lessons/manage_lesson_v2.blade.php
+++ resources/views/lessons/manage_lesson_v2.blade.php
@@ -97,22 +97,11 @@
 
 
 @section('main')
-<br><br>
-    <div class="col-md-12" >
-        {{-- BREADCRUMB --}}View Class
-        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href={{url('/home')}}>Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Class</li>
-            </ol>
-        </nav>
-    </div>
 
-    <div class="container page-inner">
+
+    <div class="page-inner">
         <div class="page-header">
-            <button type="button" class="btn btn-custom md-col-3" style="width:8%" onclick="redirectToSection('{{ url('lesson/create_v2') }}')">
-                <span>Add</span>
-            </button>
+
             <script>
                 function redirectToSection(url) {
                     window.location.href = url;
@@ -120,51 +109,67 @@
             </script>
         </div>
 
+
         <div class="col-md-12">
             <h1><strong>All Class</strong></h1>
         </div>
+
+        <div class="col-md-12" >
+            <nav >
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href={{url('/home')}}>Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Class</li>
+                </ol>
+            </nav>
+        </div>
+
+        <button type="button" class="btn btn-custom md-col-3"  onclick="redirectToSection('{{ url('lesson/create_v2') }}')">
+            <div style="margin-right: 10px; margin-left: 10px">Add</div>
+        </button>
+
+
         <div class="container-fluid mt-3 row">
             @php
                 // Ambil semua kategori pelajaran sekali
                 $lessonCategories = DB::table('lesson_categories')->get()->keyBy('name');
-                
+
             @endphp
             @forelse ($myClasses as $data)
                 @php
                 // Ambil warna kategori jika kategori ada dalam $lessonCategories
                 $warna = $lessonCategories[$data->course_category]->color_of_categories ?? '#007bff';
                 $numStudents = DB::select(
-                                "SELECT * 
+                                "SELECT *
                                     FROM
                                         student_lesson a
                                     WHERE a.lesson_id = $data->id");
                 $numStudentsCount = count($numStudents);
                 @endphp
-        
+
                 <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">
                     <div class="card shadow ">
                         <!-- Image -->
                         <img class="card-img-top"
-                             style="max-height: 200px"
+                             style="max-height: 220px"
                              onerror="this.onerror=null; this.src='{{ url('/default/default_courses.jpeg') }}'; this.alt='Course Image';"
                              src="{{ Storage::url('public/class/cover/') . $data->course_cover_image }}"
                              alt="La Noyee">
                         <!-- Card body -->
                         <div class="card-body">
                             <!-- Badge and favorite -->
-                            <div style="width: 100%; display: flex; flex-wrap: wrap; justify-content: center; align-items: center; margin-bottom: .5rem;">
+                            <div style="width: 100%; display: flex; flex-wrap: wrap; justify-content: left; align-items: flex-start; margin-bottom: .5rem;">
+                                <div class="class-badge" style="color: white; margin-bottom: 5px; margin-right: 10px; background-color: rgb(31, 65, 151); padding: 2px 10px;">
+                                    NEW
+                                </div>
                                 <div class="class-badge" style="color: white; margin-bottom: 5px; margin-right: 5px; background-color: {{ $warna }}; padding: 2px 10px;">
                                     <strong>{{ $data->course_category }}</strong>
                                 </div>
-                                <div class="class-badge" style="color: white; margin-bottom: 5px; margin-left: 5px; margin-right: 5px; background-color: rgb(31, 65, 151); padding: 2px 10px;">
-                                    NEW
-                                </div>
-                                <div class="class-badge" style="color: black; display: flex; align-items: center; margin-bottom: 5px; margin-left: 5px;">
+                                <div class="class-badge" style="color: black; display: flex; align-items: center; margin-bottom: 5px; margin-left: auto;">
                                     <img src="{{ url('/Icons/Star.svg') }}" style="margin-right: 4px;">
                                     <p style="font-size: 15px; margin-bottom: 0;"><strong>5.0</strong></p>
                                 </div>
                             </div>
-                            
+
                             <!-- Title -->
                             <h6 class="card-title"><a href="#">{{$data->course_title}}</a></h6>
                             <p class="mb-2 text-truncate-2 d-none">Proposal indulged no do sociable he throwing
@@ -173,7 +178,7 @@
 
                             <hr style="margin-left: -20px; margin-right: -20px" class="mb-3 mt-2">
 
-                            
+
                             {{-- <div class="d-flex justify-content-between">
                                 <div>
                                     <img style="width: 35%; height: auto;" src="{{ url('/HomeIcons/Toga_MDLNTraining.svg') }}">
@@ -181,7 +186,7 @@
                                 </div>
                             </div> --}}
                             <li class="toga-container dropdown hidden-caret" style="display: flex; justify-content: space-between; align-items: center;">
-                                <img style="width: 15%; height: auto;" src="{{ url('/HomeIcons/Toga_MDLNTraining.svg') }}">
+                                <img style="width: 15%; height: auto; max-height: 20px" src="{{ url('/HomeIcons/Toga_MDLNTraining.svg') }}">
                                 <p style="font-size: 15px; margin-bottom: 3px;">{{ $data->mentor_name }}</p>
                                 <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
                                     <img id="dotsThree" src="{{ url('/HomeIcons/DotsThree.svg') }}" alt="">
@@ -225,7 +230,7 @@
                                 <!-- Icon for students -->
                                 <img style="width: 10%; height: auto; margin-top: 12px;"
                                      src="{{ url('/icons/UserStudent_mentor.svg') }}">
-                                
+
                                 <!-- Link to view students -->
                                 <a style="text-decoration: none; color: black;"
                                    href="{{ url('/class/class-list/students/' . $data->id) }}">
@@ -233,20 +238,20 @@
                                         <b>{{ $numStudentsCount }}</b><span style="color: #8C94A3;"> students</span>
                                     </p>
                                 </a>
-                        
+
                                 <!-- Edit button icon -->
                                 <img style="width: 10%; height: auto; margin-top: 12px; margin-left: 10px;"
                                      src="{{ url('/icons/btn_edit.svg') }}" alt="Edit Icon">
-                                
+
                                 <!-- Delete button icon -->
                                 <img style="width: 10%; height: auto; margin-top: 12px; margin-left: 10px;"
                                      src="{{ url('/icons/btn_delete.svg') }}" alt="Delete Icon">
-                                
+
                                 <!-- Toggle switch -->
-                            
+
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
             @empty
