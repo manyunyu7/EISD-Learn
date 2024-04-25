df47a7b820ec9ea432e3493da5d9b7ab0f6212c6
Henry Augusta <henryaugusta4@gmail.com>
2024-04-25 15:23:46 +0700

    Change Profile API, Change Photo API.

---
+++ app/Http/Controllers/MobileProfileController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\MyHelper;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class MobileProfileController extends Controller
+{
+    public function updatePhoto(Request $request){
+        try {
+            // Update the user with filtered data
+            $user = User::findOrFail($request->lms_user_id);
+
+            //hapus old image
+            if ($user->profile_url != "error.png") {
+                Storage::disk('public')->delete('profile/' . $user->profile_url);
+            }
+
+            //upload new image
+            if ($request->hasFile('profile_image')) {
+                $image = $request->file('profile_image');
+                $imagePath = 'profile/' . $image->hashName();
+                Storage::disk('public')->put($imagePath, file_get_contents($image));
+                $user->profile_url = $image->hashName();
+            }
+
+            $user->save();
+
+            return MyHelper::responseSuccessWithData(
+                200,
+                200,
+                2,
+                "success",
+                "success",
+                $user
+            );
+        } catch (\Exception $e) {
+            return MyHelper::responseErrorWithData(
+                400,
+                400,
+                0,
+                "Error updating user photo",
+                $e->getMessage(),
+                null
+            );
+        }
+    }
+
+    public function updateUser(Request $request){
+        $requestData = $request->all();
+
+        // Filter out null values
+        $filteredData = array_filter($requestData, function ($value) {
+            return $value !== null;
+        });
+
+        // Update the user with filtered data
+        $user = User::findOrFail($request->lms_user_id);
+        $user->update($filteredData);
+
+        if($user){
+            return MyHelper::responseSuccessWithData(
+                200,
+                200,
+                2,
+                "success",
+                "success",
+                $user
+            );
+        } else {
+            return MyHelper::responseErrorWithData(
+                400,
+                400,
+                0,
+                "User Tidak Ditemukan",
+                "User Tidak Ditemukan",
+                null
+            );
+        }
+    }
+
+    public function getUserProfile(Request $request){
+        $userID = $request->lms_user_id;
+
+        $user  = User::find($userID);
+
+        if($user!=null){
+            return MyHelper::responseSuccessWithData(
+                200,
+                200,
+                2,
+                "success",
+                "success",
+                User::findOrFail($userID)
+            );
+        }else{
+            return MyHelper::responseErrorWithData(
+                400,
+                400,
+                0,
+                "User Tidak Ditemukan",
+                "User Tidak Ditemukan",
+                null
+            );
+        }
+
+    }
+}
