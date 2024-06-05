13f3c6b8ef926f2291c084dcff5d9a1ce762c98c
KevinValerian21 <kevinvalerianinia@gmail.com>
2024-06-05 14:26:41 +0700

    Add Logo baru

--- resources/views/landing168.blade.php
+++ resources/views/landing168.blade.php
@@ -486,8 +486,6 @@
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
                 </div>
             </div>
