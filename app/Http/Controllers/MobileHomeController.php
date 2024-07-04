fed03dded77de9196fcc0a749a9b0ac28d5a9595
Henry Augusta <henryaugusta4@gmail.com>
2024-07-04 09:57:39 +0700

    import stdclass to MobileHomeController

--- app/Http/Controllers/MobileHomeController.php
+++ app/Http/Controllers/MobileHomeController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
+use stdClass;
 
 class MobileHomeController extends Controller
 {
