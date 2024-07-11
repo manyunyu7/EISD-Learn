94b55290d1f43e6b1253b5148c14b0f15750512f
Henry Augusta <henryaugusta4@gmail.com>
2024-07-11 15:35:50 +0700

    fix query for first_section of each classes.

--- app/Http/Controllers/MyClassController.php
+++ app/Http/Controllers/MyClassController.php
@@ -21,75 +21,87 @@ use Illuminate\Support\Facades\DB;
 
 class MyClassController extends Controller
 {
-    public function myClass()
+    public function myClass(Request $request)
     {
         $userID = Auth::id();
-        $sortParam = request('sort' , 'Latest') ;
-        $catParam  = request('category', 'All Category');
+        $sortParam = $request->input('sort', 'Latest');
+        $catParam = $request->input('category', 'All Category');
 
         $lessonCategories = LessonCategory::all();
 
         $sortBy = ($sortParam == 'Latest') ? 'desc' : 'asc';
-        $catBy = ($catParam == 'All Category') ? '*' : $catParam;
 
-        // Membuat subquery menggunakan Query Builder
         $subQuery = DB::table('lessons as a')
-        ->select([
-            DB::raw('RANK() OVER (PARTITION BY a.id ORDER BY cs.id ASC) AS ranking'),
-            'a.*',
-            'lc.color_of_categories as course_category_color',
-            'lc.name as course_category_name',
-            'b.name as mentor_name',
-            'b.profile_url',
-            DB::raw('COUNT(c.student_id) AS num_students_registered'),
-            DB::raw('CASE WHEN COUNT(c.student_id) > 0 THEN 1 ELSE 0 END AS is_registered')
-        ])
-        ->leftJoin('users as b', 'a.mentor_id', '=', 'b.id')
-        ->leftJoin('student_lesson as c', 'a.id', '=', 'c.lesson_id')
-        ->leftJoin('course_section as cs', 'a.id', '=', 'cs.course_id')
-        ->leftJoin('lesson_categories as lc', 'a.category_id', '=', 'lc.id')
-        ->whereExists(function ($query) use ($userID) {
-            $query->select(DB::raw(1))
-                ->from('student_lesson as sl')
-                ->whereRaw('sl.lesson_id = a.id')
-                ->where('sl.student_id', $userID);
-        })
-        ->groupBy('a.id', 'b.name', 'b.profile_url', 'cs.id');
+            ->select([
+                DB::raw('RANK() OVER (PARTITION BY a.id ORDER BY cs.id ASC) AS ranking'),
+                'a.*',
+                'lc.color_of_categories as course_category_color',
+                'lc.name as course_category_name',
+                'b.name as mentor_name',
+                'b.profile_url',
+                DB::raw('COUNT(c.student_id) AS num_students_registered'),
+                DB::raw('CASE WHEN COUNT(c.student_id) > 0 THEN 1 ELSE 0 END AS is_registered')
+            ])
+            ->leftJoin('users as b', 'a.mentor_id', '=', 'b.id')
+            ->leftJoin('student_lesson as c', 'a.id', '=', 'c.lesson_id')
+            ->leftJoin('course_section as cs', 'a.id', '=', 'cs.course_id')
+            ->leftJoin('lesson_categories as lc', 'a.category_id', '=', 'lc.id')
+            ->whereExists(function ($query) use ($userID) {
+                $query->select(DB::raw(1))
+                    ->from('student_lesson as sl')
+                    ->whereRaw('sl.lesson_id = a.id')
+                    ->where('sl.student_id', $userID);
+            })
+            ->groupBy('a.id', 'b.name', 'b.profile_url', 'cs.id');
 
+        $myClassesQuery = DB::table(DB::raw("({$subQuery->toSql()}) as main_table"))
+            ->mergeBindings($subQuery)
+            ->where('main_table.ranking', 1)
+            ->when($sortParam == 'Latest', function ($query) use ($sortBy) {
+                return $query->orderBy('main_table.created_at', $sortBy);
+            }, function ($query) use ($sortBy) {
+                return $query->orderBy('main_table.num_students_registered', $sortBy);
+            })
+            ->when($catParam != 'All Category', function ($query) use ($catParam) {
+                return $query->where('main_table.course_category_name', $catParam);
+            });
 
-        $myClasses = DB::table(DB::raw("({$subQuery->toSql()}) as main_table"))
-        ->mergeBindings($subQuery) // Menggabungkan bindings dari subquery
-        ->where('main_table.ranking', 1)
-        ->when($sortParam == 'Latest', function ($query) use ($sortBy) {
-            return $query->orderBy('main_table.created_at', $sortBy);
-        }, function ($query) use ($sortBy) {
-            return $query->orderBy('main_table.num_students_registered', $sortBy);
-        })
-        ->when($catParam != 'All Category', function ($query) use ($catParam) {
-            return $query->where('main_table.course_category_name', $catParam);
-        })
-        ->get();
-        // Append new attribute to each row
+        $myClasses = $myClassesQuery->get();
 
         foreach ($myClasses as &$class) {
-            $firstSection = DB::table('course_section')
-                ->where('course_id', '=', $class->id)
-                ->orderByRaw("CAST(section_order AS UNSIGNED), section_order ASC")
-                ->first();
+            $sections = CourseSection::select(
+                'lessons.id as lesson_id',
+                'lessons.course_title as lessons_title',
+                'lessons.mentor_id',
+                'users.name as mentor_name',
+                'course_section.id as section_id',
+                'course_section.section_order',
+                'course_section.section_title',
+                'course_section.quiz_session_id',
+                'course_section.section_content',
+                'course_section.section_video',
+                'course_section.created_at',
+                'course_section.updated_at',
+                'course_section.can_be_accessed'
+            )
+            ->leftJoin('lessons', 'lessons.id', '=', 'course_section.course_id')
+            ->leftJoin('users', 'users.id', '=', 'lessons.mentor_id')
+            ->where('course_section.course_id', $class->id)
+            ->orderBy(DB::raw('CAST(course_section.section_order AS UNSIGNED)'), 'ASC')
+            ->first();
 
-            // Tambahkan pengecekan untuk memastikan $firstSection tidak null
-            if ($firstSection !== null) {
-                $class->first_section = $firstSection->id;
-            } else {
-                // Handle jika $firstSection null
-                $class->first_section = null; // Atau nilai default lainnya sesuai kebutuhan
-            }
+            $class->first_section = $sections ? $sections->section_id : null;
         }
 
+        $compact = compact('myClasses', 'userID', 'lessonCategories');
 
-        // return $class->first_section;
-        // return $myClasses;
+        if ($request->dump == true) {
+            return $compact;
+        }
 
-        return view('student.myclass')->with(compact('myClasses', 'userID', 'lessonCategories'));
+        return view('student.myclass')->with($compact);
     }
+
+
+
 }
